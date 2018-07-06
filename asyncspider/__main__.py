88f40c5a01798395d223eef761c9927c8d7637a0
Matthias Riegler <riegler.matthias2@gmail.com>
2018-07-06 15:00:29 +0200

    Added CLI

--- asyncspider/__main__.py
+++ asyncspider/__main__.py
@@ -8,29 +8,62 @@
     :copyright: (c) 2018, Matthias Riegler,
     :license: GPLv3, see LICENSE.md for more details.
 """
+import aioredis
+import asyncio
 import logging
 import multiprocessing as mp
 import uvloop
+import click
+from yarl import URL
 from . import Spider
 
+
+# Get logger
+logger = logging.getLogger(__name__)
 
 # Set loglevel to info
 logging.basicConfig(level=logging.INFO)
 
 
-def create_spider():
-    loop = uvloop.new_event_loop()
-    Spider(loop=loop, num_tasks=1)
+@click.command()
+@click.option("--num-threads",
+              default=mp.cpu_count(),
+              help="Number of multiprocessing threads")
+@click.option("--num-tasks",
+              default=10,
+              help="Number of async crawlers for each thread")
+@click.option("--redis-url",
+              default="redis://localhost",
+              help="Redis URL, you have to pass a common URL on each node if" +
+                   " you want to run the crawler in a distributed manner")
+@click.option("--start-url",
+              default="",
+              help="Where should we start..? :-)")
+def run(num_threads, num_tasks, redis_url, start_url):
+    """ Distributed web crawler based on uvloop, aiohttp and redis """
 
-    return mp.Process(target=loop.run_forever)
+    async def add_start_url():
+        """ Sets a start URL if requested """
+        redis = await aioredis.create_redis(redis_url)
+        await redis.sadd(Spider.TO_CRAWL_URLS, start_url)
+        logger.info(f"Added {start_url} to the queue")
 
-
-if __name__ == "__main__":
+    def create_spider():
+        """ Creats a spider object and sets the multiprocessing target """
+        loop = uvloop.new_event_loop()
+        Spider(loop=loop,
+               num_tasks=num_tasks,
+               redis_url=redis_url)
+        return mp.Process(target=loop.run_forever)
 
     processes = []
 
-    for _ in range(mp.cpu_count()):
+    for _ in range(num_threads):
         processes.append(create_spider())
+
+    # Set start URL
+    if URL(start_url).is_absolute():
+        asyncio.get_event_loop().run_until_complete(add_start_url())
 
     for process in processes:
         process.start()
@@ -38,3 +71,8 @@ if __name__ == "__main__":
     input(" -> Press enter to exit")
     for process in processes:
         process.terminate()
+
+
+# Entrypoint
+if __name__ == "__main__":
+    run()
